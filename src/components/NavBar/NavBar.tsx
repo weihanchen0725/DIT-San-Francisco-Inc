09790725b21805d96b9ada4428d1c3dabb4ea05b
WeiHanChen <weihanchen0725@gmail.com>
2026-01-09 18:42:49 -0800

    Refactor code structure for improved readability and maintainability

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
+import CompanyLogo from '../../assets/images/tcm_logistic_logo.svg'
 
 const NavBar = () => {
     return (
         <>
-        NavBar
+        <Image src={CompanyLogo} alt="Company Logo" />  
         <Link href='/'>Home</Link>
         <Link href="/about">About</Link>
         <Link href="/services">Services</Link>
